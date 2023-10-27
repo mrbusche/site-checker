a079885227f717ecde0cb6bf7fd393e4157efaf4
Matt Busche <mrbusche@gmail.com>
2023-10-26 22:09:30 -0500

    test on failure

--- cypress/e2e/website.cy.js
+++ cypress/e2e/website.cy.js
@@ -2,7 +2,7 @@ describe('check website', () => {
   it('verifies data', () => {
     Cypress.config('defaultCommandTimeout', 10000);
     cy.visit(Cypress.env('url'));
-    cy.contains('No times in October');
+    cy.contains('No times in Octoberrr');
     cy.contains('View next month').click();
     cy.contains('No times in November');
     cy.contains('View next month').click();
