874c59dbae7de5b76e62d36af2badb41f29075ba
Matt Busche <mrbusche@gmail.com>
2023-10-28 09:26:44 -0500

    increase timeout

--- cypress/e2e/website.cy.js
+++ cypress/e2e/website.cy.js
@@ -1,6 +1,5 @@
 describe('check website', () => {
   it('verifies data', () => {
-    Cypress.config('defaultCommandTimeout', 10000);
     cy.visit(Cypress.env('url'));
     cy.contains('No times in October');
     cy.contains('View next month').click();
